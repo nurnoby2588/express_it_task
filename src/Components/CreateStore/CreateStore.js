720cb61fca2c7fdff6e2cb45c4a449147e59b0c2
nurnoby admin <nurnoby323@gmail.com>
2025-02-20 19:34:27 +0600

    rsponsive done

--- src/Components/CreateStore/CreateStore.js
+++ src/Components/CreateStore/CreateStore.js
@@ -112,12 +112,15 @@ const CreateStore = () => {
     console.log(isValidDomain.length)
     return (
         <div>
-            <form style={{maxHeight:'760px', position:'fixed',top:"0px",bottom:'0px', left:'0px',right:'0px' , margin:'auto' }} className='w-[1200px]  rounded-sm shadow-md bg-slate-100  mx-auto p-5' action={hanldeSubmit}>
+            <form style={{ position:'fixed',top:"0px",bottom:'0px', left:'0px',right:'0px' , margin:'auto' , overflow:'auto'}} className=' md:w-[650px] lg:w-[950px] lg:h-[710px] w-[1200px] xl:h-[710px] md:h-[700px] sm:h-[750px] h-[700px] rounded-sm shadow-md bg-slate-100  mx-auto p-5' action={hanldeSubmit}>
                 <h3 className="text-2xl font-semibold mb-5">Create a Store</h3>
-                <p>Add your basic store information and  complete the setup</p>
+                <div  className="sm:w-full w-3/12">
+                <p className="">Add your basic store information and  complete the setup</p>
+                </div>
+               
                 <hr className='mb-5 mt-1' />
 
-                <div className='grid lg:grid-cols-2 p-3'>
+                <div className='grid  lg:grid-cols-2 sm:grid-cols-1 p-3'>
                     <div className='flex flex-row'>
                         <div className=' flex-none w-[40px]'>
                             <  AiOutlineDesktop className='text-blue-300' style={{ fontSize: '30px' }} />
@@ -125,13 +128,13 @@ const CreateStore = () => {
                         <div className='flex-1 px-2'>
                             <div>
                                 <h4 className='font-bold'>Give your online store a name</h4>
-                                <small>A great store name is big part of your success. Make sure it aligns with your brand and products</small>
+                                <small className='hidden sm:block'>A great store name is big part of your success. Make sure it aligns with your brand and products</small>
                             </div>
                         </div>
                     </div>
-                    <div className=' p-3'>
+                    <div className=' pl-3'>
                         <div>
-                            <input style={{ outline: 'none' }} className={!isValidName ? "w-full rounded border border-gray-500 focus:ring-blue-500   px-3 py-2 " : "w-full rounded border border-red-500 focus:ring-red-500  px-3 py-2 "}
+                            <input style={{ outline: 'none' }} className={!isValidName ? " w-fit sm:w-2/4 xl:w-full lg:w-11/12 md:w-full  rounded border border-gray-300 focus:ring-blue-500 focus:border-blue-500  px-3 py-2 " : "w-fit sm:w-2/4 xl:w-full lg:w-11/12 md:w-full rounded border border-red-500 focus:ring-red-500  px-3 py-2 "}
 
                                 placeholder="How'd you like to call you store?" type="text" name='name' onChange={handleStoreName} />
                         </div>
@@ -153,14 +156,14 @@ const CreateStore = () => {
                         <div className='flex-1 px-2'>
                             <div>
                                 <h4 className='font-bold'>Your online store subdomain</h4>
-                                <small>A SEO-friendly store name is a crucial part of your success. Make sure it
+                                <small className='hidden sm:block'>A SEO-friendly store name is a crucial part of your success. Make sure it
                                     aligns with your brand and products.</small>
                             </div>
                         </div>
                     </div>
-                    <div className=' p-3'>
+                    <div className=' pl-3'>
                         <div>
-                            <input style={{ outline: 'none' }} className={!isValidDomain ? "w-full rounded border border-gray-500 focus:ring-blue-500   px-3 py-2 " : "w-full rounded border border-red-500 focus:ring-red-500  px-3 py-2 "} placeholder="Enter your domain name" type="text" name='domain' onChange={handleValidDomin} />
+                            <input style={{ outline: 'none' }} className={!isValidDomain ? "xw-fit sm:w-2/4 xl:w-full lg:w-11/12 md:w-full rounded border border-gray-300 focus:ring-blue-500 focus:border-blue-500   px-3 py-2 " : "xw-fit sm:w-2/4 xl:w-full lg:w-11/12 md:w-full rounded border border-red-500 focus:ring-red-500  px-3 py-2 "} placeholder="Enter your domain name" type="text" name='domain' onChange={handleValidDomin} />
                         </div>
                         <div>
                             {
@@ -181,15 +184,15 @@ const CreateStore = () => {
                         <div className='flex-1 px-2'>
                             <div>
                                 <h4 className='font-bold'>Where's your store located?</h4>
-                                <small>Set your store's default location so we can optimize store access and
+                                <small className='hidden sm:block'>Set your store's default location so we can optimize store access and
                                     speed for your customers.</small>
                             </div>
                         </div>
                     </div>
                     <div>
-                        <div className="p-3">
+                        <div className="pl-3">
                             {/* <label for="countries" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Select an option</label> */}
-                            <select name="location" id="location" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+                            <select name="location" id="location" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-fit sm:w-2/4 xl:w-full lg:w-11/12 md:w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
                                 <option selected>Choose a country</option>
                                 <option value="Bangladesh">Bangladesh</option>
 
@@ -218,15 +221,15 @@ const CreateStore = () => {
                         <div className='flex-1 px-2'>
                             <div>
                                 <h4 className='font-bold'>What's your Category?</h4>
-                                <small>Set your store's default category so that we can optimize store access
+                                <small className='hidden sm:block'>Set your store's default category so that we can optimize store access
                                     and speed for your customers.</small>
                             </div>
                         </div>
                     </div>
                     <div>
-                        <div className="p-3">
+                        <div className="pl-3">
                             {/* <label for="countries" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Select an option</label> */}
-                            <select name="category" id="category" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+                            <select name="category" id="category" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-fit sm:w-2/4 xl:w-full lg:w-11/12 md:w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
                                 <option selected>Choose a category</option>
                                 <option value="Fashion">Fashion</option>
 
@@ -252,14 +255,14 @@ const CreateStore = () => {
                         <div className='flex-1 px-2'>
                             <div>
                                 <h4 className='font-bold'>Choose store currency</h4>
-                                <small>This is the main currency you wish to sell in.</small>
+                                <small className='hidden sm:block'>This is the main currency you wish to sell in.</small>
                             </div>
                         </div>
                     </div>
                     <div>
-                        <div className="p-3">
+                        <div className="pl-3">
                             {/* <label for="countries" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Select an option</label> */}
-                            <select name="currency" id="currency" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+                            <select name="currency" id="currency" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-fit sm:w-2/4 xl:w-full lg:w-11/12 md:w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
                                 <option selected>Choose a currency</option>
                                 <option value="BDT">BDT(Taka)</option>
 
@@ -281,14 +284,14 @@ const CreateStore = () => {
                         <div className='flex-1 px-2'>
                             <div>
                                 <h4 className='font-bold'>Store contact email</h4>
-                                <small>This is the email you'll use to send notifications to and receive orders
+                                <small className='hidden sm:block'>This is the email you'll use to send notifications to and receive orders
                                     from customers.</small>
                             </div>
                         </div>
                     </div>
-                    <div className='p-3'>
+                    <div className='pl-3'>
                         <div>
-                            <input style={{ outline: 'none' }} className={isValidEmail ? "w-full rounded border border-gray-500 focus:ring-blue-500   px-3 py-2 " : "w-full rounded border border-red-500 focus:ring-red-500  px-3 py-2 "} name='email' placeholder='you@example.com' type="email" onChange={handleEmail} />
+                            <input style={{ outline: 'none' }} className={isValidEmail ? " w-fit sm:w-2/4 xl:w-full lg:w-11/12 md:w-full rounded border border-gray-300 focus:ring-blue-500 focus:border-blue-500  px-3 py-2 " : "w-fit sm:w-2/4 xl:w-full lg:w-11/12 md:w-full rounded border border-red-500 focus:ring-red-500  px-3 py-2 "} name='email' placeholder='you@example.com' type="email" onChange={handleEmail} />
                         </div>
                         <div>
                             {
@@ -299,7 +302,7 @@ const CreateStore = () => {
 
                     </div>
                 </div>
-                <div className='flex justify-end p-3'>
+                <div className='flex  justify-srart md:justify-center lg:justify-end xl:justify-end p-3'>
                     <input className={`  px-6 py-2 text-white font-semibold rounded-lg transition duration-300 ${(isValidName == false && isValidEmail == true && errorDominMegs == false)
                         ? "bg-gray-400 cursor-not-allowed"
                         : "bg-blue-500 hover:bg-blue-600"
