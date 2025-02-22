6632158dcd9792bbc8068d726a10239244471736
nurnoby admin <nurnoby323@gmail.com>
2025-02-22 11:17:11 +0600

    remove poxey

--- src/api/proxy.js
+++ src/api/proxy.js
@@ -1,16 +1,16 @@
-export default async function handler(req, res) {
-    try {
-        const response = await fetch("https://glore-bd-backend-node-mongo.vercel.app/api/product", {
-            method: "GET",
-            headers: {
-                "Content-Type": "application/json"
-            }
-        });
+// export default async function handler(req, res) {
+//     try {
+//         const response = await fetch("https://glore-bd-backend-node-mongo.vercel.app/api/product", {
+//             method: "GET",
+//             headers: {
+//                 "Content-Type": "application/json"
+//             }
+//         });
 
-        const data = await response.json();
-        res.setHeader("Access-Control-Allow-Origin", "*");
-        res.status(200).json(data);
-    } catch (error) {
-        res.status(500).json({ error: "Proxy failed" });
-    }
-}
+//         const data = await response.json();
+//         res.setHeader("Access-Control-Allow-Origin", "*");
+//         res.status(200).json(data);
+//     } catch (error) {
+//         res.status(500).json({ error: "Proxy failed" });
+//     }
+// }
